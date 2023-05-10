e2411b1d5a98e1eb0e80fbde6c838984172e3738
fvr124 <fvr124@dandycomp03fl.unicph.domain>
2023-05-10 16:33:17 +0200

    Updated compressbam, seems to work quite well

--- misc/compressbam.cpp
+++ misc/compressbam.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <cstring>
 #include <cstdlib>
+#include <iostream>//for printing time
 #include <htslib/sam.h>
 #include <htslib/thread_pool.h>
 #include <htslib/bgzf.h>
@@ -10,11 +11,14 @@
 #include <ctype.h>
 #include <htslib/kstring.h>
 #include <htslib/hfile.h>
-#include "../shared.h"
+
+//#include "../shared.h"
+
+#define VERSION "0.1"
 
 htsFormat *dingding2 =(htsFormat*) calloc(1,sizeof(htsFormat));
 htsThreadPool p = {NULL, 0};
-int nthreads = 4;
+int nthreads = 8;
 char out_mode[5]="ws";
 
 //get ref used not get refused
@@ -36,14 +40,14 @@ size_t getrefused(samFile *htsfp,bam_hdr_t *hdr,int *keeplist,int &nkeep){
   for(int i=0;i<sam_hdr_nref(hdr);i++)
     if(keeplist[i]!=-1)
       nkeep++;
-  fprintf(stderr,"\n\t-> Done reading list of refids to keep %d\n",nkeep);
+  fprintf(stderr,"\n\t-> Done reading list of refids: to keep %d\n",nkeep);
   bam_destroy1(aln);
   return at;
 }
 
-void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp){
+void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp,char *mycl){
   BGZF *fp = NULL;
-  fp=bgzf_open(outfile,"wb");
+  fp=bgzf_open(outfile,"w5");//write bgzf with compression level5. Maybe this should be changed.
   bgzf_mt(fp,nthreads,256);
   assert(fp!=NULL);
   
@@ -63,6 +67,11 @@ void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp){
     assert(sam_hdr_find_line_pos(hdr,"SQ",i,&kstmp)==0);
     kputs(kstmp.s,&newhdr_ks);
     kputc('\n',&newhdr_ks);
+    if(newhdr_ks.l>10000000){
+      assert(bgzf_write(fp,newhdr_ks.s,newhdr_ks.l)==newhdr_ks.l);
+      newhdr_ks.l =0;
+    }
+    
   }
 
   for(int i=0;i<sam_hdr_count_lines(hdr,"RG");i++){
@@ -93,7 +102,7 @@ void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp){
   free(newhdr_ks.s);
   free(kstmp.s);
   bgzf_close(fp);
- 
+  fprintf(stderr,"\t-> Done writing new header as text\n");fflush(stderr);
 
   samFile *tmpfp = NULL;
   tmpfp = hts_open(outfile,"r");
@@ -104,9 +113,10 @@ void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp){
   sam_close(tmpfp);
 
   //fprintf(stderr,"newhdr: %s\n",sam_hdr_str(newhdr));
-  fprintf(stderr,"\t header info before: nref: %d bytesize: %lu\n",sam_hdr_nref(hdr),sam_hdr_length(hdr));
-  fprintf(stderr,"\t header info before: nref: %d bytesize: %lu\n",sam_hdr_nref(newhdr),sam_hdr_length(newhdr));
-  
+  fprintf(stderr,"\t-> header info before: nref: %d bytesize: %lu\n",sam_hdr_nref(hdr),sam_hdr_length(hdr));
+  fprintf(stderr,"\t-> header info  after: nref: %d bytesize: %lu\n",sam_hdr_nref(newhdr),sam_hdr_length(newhdr));
+  fprintf(stderr,"\t-> header info reduction nref: %f bytesize: %f\n",(double)sam_hdr_nref(newhdr)/sam_hdr_nref(hdr),(double)sam_hdr_length(newhdr)/sam_hdr_length(hdr));
+  fflush(stderr);
   //write outputfile
   samFile *outhts = NULL;
   if ((outhts = sam_open_format(outfile,out_mode, dingding2)) == 0) {
@@ -127,34 +137,46 @@ void writemod(const char *outfile ,bam_hdr_t *hdr,int *keeplist,samFile *htsfp){
     keeplist[i] = sam_hdr_name2tid(newhdr,sam_hdr_tid2name(hdr,i));
     assert(keeplist[i]>=0);
   }
-  
-  int ret = sam_hdr_write(outhts,newhdr);
+  sam_hdr_add_pg(newhdr,"compressbam","VN",VERSION,"CL",mycl,NULL);
 
+  int ret = sam_hdr_write(outhts,newhdr);
+  fprintf(stderr,"\t-> Done writing new header as binary\n");fflush(stderr);
   //now mainloop
   bam1_t *aln = bam_init1();
   size_t at =1;
   while(sam_read1(htsfp,hdr,aln) >= 0) {
-    if((at++ %100000)==0  )
+    if((at++ %100000)==0  ){
       fprintf(stderr,"\r\t->  Now at read:     %lu ",at);
-    int oldchr = aln->core.tid ; //contig name (chromosome)
-    aln->core.tid = keeplist[oldchr];
+      sam_flush(htsfp);
+      fflush(stderr);
+    }
+
+    aln->core.tid = keeplist[aln->core.tid];
     if(aln->core.mtid!=-1)
       aln->core.mtid = keeplist[aln->core.mtid];
     assert(sam_write1(outhts, newhdr,aln)>=0);
   }
-  
+  sam_flush(htsfp);
   sam_hdr_destroy(newhdr);
   bam_destroy1(aln);
   sam_close(outhts);
-  //  hts_tpool_destroy(p.pool);
+  hts_tpool_destroy(p.pool);
+
 }
 
 int main(int argc,char**argv){
-  
+
+  //print time
+  clock_t t=clock();
+  time_t t2=time(NULL);
+
   if(argc==1){
-    fprintf(stderr,"./compressbam -hts -ref -out -type [Sam,Cram,Bam]\n");
+    fprintf(stderr,"./compressbam -hts -ref -out -type [Sam,Cram,Bam] -@ nthreads\n");
     return 0;
   }
+  char *mycl = stringify_argv(argc,argv);
+  fprintf(stderr,"\t-> compressbam: (%s;%d;%s): \'%s\'\n",__FILE__,__DATE__,__TIME__,mycl);  
+  
   argv++;
   char *hts = NULL;
   char *names = NULL;
@@ -168,7 +190,11 @@ int main(int argc,char**argv){
     char *val=*(++argv);
     //fprintf(stderr,"key: %s val: %s\n",key,val);
     if(!strcasecmp("-hts",key)) hts=strdup(val);
-    else if(!strcasecmp("-out",key)) outfile=strdup(val);
+    else if(!strcasecmp("-out",key))
+      {
+	free(outfile);
+	outfile=strdup(val);
+      }
     else if(!strcasecmp("-ref",key)) ref=strdup(val);
     else if(!strcasecmp("-@",key)) nthreads=atoi(val);
     else if(!strcasecmp("-type",key)){
@@ -205,8 +231,9 @@ int main(int argc,char**argv){
   sam_hdr_destroy(hdr);
   htsfp=hts_open(hts,"r" );
   hdr = sam_hdr_read(htsfp);
-  
-  writemod(outfile,hdr,keeplist,htsfp);
+  writemod(outfile,hdr,keeplist,htsfp,mycl);
+  fprintf(stderr,"\t-> Done writing file: \'%s\'\n",hts);
+  free(mycl);
   sam_close(htsfp);
 
   if(hts) free(hts);
@@ -215,6 +242,8 @@ int main(int argc,char**argv){
 
   free(dingding2);
   sam_hdr_destroy(hdr);
+  delete [] keeplist;
 
+  fprintf(stderr, "\t-> [ALL done] cpu-time used =  %.2f sec walltime used =  %.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC, (float)(time(NULL) - t2));
   return 0;
 }
