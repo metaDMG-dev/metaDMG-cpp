8d28d337aa3d954c6ef5a47cc984057b71d3dcc4
Thorfinn <thorfinn.sand@gmail.com>
2023-07-23 20:19:09 +0200

    Updated dfit output names. Switched to bgzf. Updated print options

--- main_dfit.cpp
+++ main_dfit.cpp
@@ -5,9 +5,9 @@
  */
 
 #include <cstdio>
-#include <zlib.h>
 #include <htslib/hts.h>   // for htsFormat, hts_opt_add, htsFile, hts_opt
 #include <htslib/sam.h>   // for htsFormat, hts_opt_add, htsFile, hts_opt
+#include <htslib/bgzf.h>
 
 #include "profile.h"
 #include "shared.h"
@@ -46,8 +46,9 @@ void make_dfit_format(mydataD &md,double **dat,int howmany){
 
 mydataD getval_full(std::map<int, mydataD> &retmap, int2intvec &child, int taxid, int howmany);
 mydata2 getval_stats(std::map<int, mydata2> &retmap, int2intvec &child, int taxid) ;
+
 int main_dfit(int argc, char **argv) {
-    fprintf(stderr, "./metaDMG-cpp dfit file.bdamage.gz -names file.gz -nodes trestructure.gz -lcastat fil.gz -bam file.bam -showfits int -nopt int\n");
+    fprintf(stderr, "./metaDMG-cpp dfit file.bdamage.gz -names file.gz -nodes trestructure.gz -lcastat fil.gz -bam file.bam -showfits int -nopt int -out file\n");
     if (argc <= 1)
         return 0;
     char *infile_bdamage = NULL;
@@ -55,6 +56,7 @@ int main_dfit(int argc, char **argv) {
     char *infile_names = NULL;
     char *infile_lcastat = NULL;
     char *infile_bam = NULL;
+    char *outfile_name = NULL;
     int howmany;//this is the cycle
     int showfits=0;
     int nopt = 5;
@@ -67,6 +69,8 @@ int main_dfit(int argc, char **argv) {
             infile_lcastat = strdup(*(++argv));
         else if (strcasecmp("-bam", *argv) == 0)
             infile_bam = strdup(*(++argv));
+	 else if (strcasecmp("-out", *argv) == 0)
+            outfile_name = strdup(*(++argv));
 	else if (strcasecmp("-nopt", *argv) == 0)
 	  nopt = atoi(*(++argv));
 	else if (strcasecmp("-showfits", *argv) == 0)
@@ -87,13 +91,19 @@ int main_dfit(int argc, char **argv) {
         }
         hdr = sam_hdr_read(samfp);
     }
-
-    fprintf(stderr, "infile_names: %s infile_bdamage: %s nodes: %s lca_stat: %s infile_bam: %s showfits: %d nopt: %d ", infile_names, infile_bdamage, infile_nodes, infile_lcastat, infile_bam,showfits,nopt);
+    if(infile_bam!=NULL&&infile_nodes!=NULL){
+      fprintf(stderr,"\t-> Potential issue, supplying both -bam and -nodes might not be meaningfull\n");
+      exit(1);
+    }
+    
+    fprintf(stderr, "infile_names: %s infile_bdamage: %s nodes: %s lca_stat: %s infile_bam: %s showfits: %d nopt: %d outname: %s ", infile_names, infile_bdamage, infile_nodes, infile_lcastat, infile_bam,showfits,nopt,outfile_name);
     fprintf(stderr, "#VERSION:%s\n", METADAMAGE_VERSION);
+    if(outfile_name==NULL)
+      outfile_name = strdup(infile_bdamage);
     char buf[1024];
-    snprintf(buf, 1024, "%s.dfit.gz", infile_bdamage);
+    snprintf(buf, 1024, "%s.dfit.txt.gz", outfile_name);
     fprintf(stderr, "\t-> Dumping file: \'%s\'\n", buf);
-    gzFile fpfpfp = gzopen(buf, "wb");
+    BGZF *fpfpfp = bgzf_open(buf, "wb");
     kstring_t *kstr = new kstring_t;
     kstr->s = NULL; kstr->l = kstr->m = 0;
     // map of taxid -> taxid
@@ -127,24 +137,36 @@ int main_dfit(int argc, char **argv) {
     dat[2] = new double [2*howmany];
     fprintf(stderr,"\t-> Will do optimization of %lu different taxids/chromosomes/scaffolds\n",retmap.size());
     if(showfits==0){
-      ksprintf(kstr,"A\tq\tc\tphi\tllh\tncall\tZ\tsign\n");
+      ksprintf(kstr,"id\tA\tq\tc\tphi\tllh\tncall\tZ\tsign\n");
     }else{
-      ksprintf(kstr,"A\tq\tc\tphi\tllh\tncall\tZ\tsign");
+      ksprintf(kstr,"id\tA\tq\tc\tphi\tllh\tncall\tZ\tsign");
       for(int i=0;i<howmany;i++)
 	ksprintf(kstr,"\tfwK%d\tfwN%d\tfwdx%d\tfwdxConf%d",i,i,i,i);
       for(int i=0;i<howmany;i++)
 	ksprintf(kstr,"\tbwK%d\tbwN%d\tbwdx%d\tbwtdxConf%d",i,i,i,i);
       ksprintf(kstr,"\n");
     }
+
     for (std::map<int, mydataD>::iterator it = retmap.begin(); it != retmap.end(); it++) {
         int taxid = it->first;
         mydataD md = it->second;
         if (it->second.nreads == 0)
             continue;
 
-	if(hdr==NULL){
-	  int taxid_id_refid =1;
-	  //gzprintf(fpfpfp, "%s\t5'\t%d", sam_hdr_tid2name(hdr, taxid), i);
+	if(hdr!=NULL){
+	  ksprintf(kstr, "%s\t", sam_hdr_tid2name(hdr, it->first));
+	}else{
+	  if(name_map.size()==0)
+	    ksprintf(kstr,"%d\t",it->first);
+	  else{
+	    int2char::iterator nit = name_map.find(it->first);
+	    if(nit==name_map.end()){
+	      fprintf(stderr,"\t-> Problem finding taxid: %d in names database: %s\n",it->first,infile_names);
+	      exit(1);
+	    }
+	    ksprintf(kstr,"%d:%s\t",it->first,nit->second);
+	  }
+	    
 	}
 
 	make_dfit_format(md,dat,howmany);
@@ -172,13 +194,14 @@ int main_dfit(int argc, char **argv) {
 	}
 	ksprintf(kstr,"\n");
     }
-    gzwrite(fpfpfp,kstr->s,kstr->l);
+    bgzf_write(fpfpfp,kstr->s,kstr->l);
     kstr->l = 0;
-    gzclose(fpfpfp);
+    bgzf_close(fpfpfp);
+    fpfpfp = NULL;
     if(infile_lcastat){
-      snprintf(buf, 1024, "%s.dfit.stat.txt.gz", infile_bdamage);
+      snprintf(buf, 1024, "%s.dfit.stat.txt.gz", outfile_name);
       fprintf(stderr, "\t-> Dumping file: \'%s\'\n", buf);
-      fpfpfp = gzopen(buf, "wb");
+      fpfpfp = bgzf_open(buf, "wb");
       ksprintf(kstr, "#taxid\tname\trank\tnalign\tnreads\tmean_rlen\tvar_rlen\tmean_gc\tvar_gc\n");
     }
     std::map<int, mydata2> stats;
@@ -217,10 +240,11 @@ int main_dfit(int argc, char **argv) {
             //      fprintf(stderr,"%d->(%d,%f,%f,%f,%f)\n",it->first,it->second.nreads,it->second.data[0],it->second.data[1],it->second.data[2],it->second.data[3]);
         }
     }
-    gzwrite(fpfpfp,kstr->s,kstr->l);
-    
     //cleanup
-    gzclose(fpfpfp);
+    if(fpfpfp){
+      bgzf_write(fpfpfp,kstr->s,kstr->l);
+      bgzf_close(fpfpfp);
+    }
     for(int2char::iterator it=name_map.begin();it!=name_map.end();it++)
       free(it->second);
     for(int2char::iterator it=rank.begin();it!=rank.end();it++)
