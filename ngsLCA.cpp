901c89a30d6f8852d7f7944820eb03243a93d34e
Your Name <you@example.com>
2026-04-26 15:32:43 +0200

    very small typo fix

--- ngsLCA.cpp
+++ ngsLCA.cpp
@@ -659,7 +659,7 @@ void hts(gzFile fp, samFile *fp_in, int2int &ref2tax, int2int &parent, bam_hdr_t
         if (it == ref2tax.end() || dingdong == -1) {
 	  int2int::iterator it2missing = i2i_missing.find(chr);
             if (it2missing == i2i_missing.end()) {
-                fprintf(stderr, "[log] \t-> problem finding chrid:%d chrname:%s (due to problem with missing entries in nodes.dmp or supporting files)\n", chr, hdr->target_name[chr]);
+                fprintf(stderr, "[log] \t-> problem finding id:%d chrname:%s (due to problem with missing entries in nodes.dmp or supporting files)\n", chr, hdr->target_name[chr]);
                 i2i_missing[chr] = 1;
             } else
                 it2missing->second = it2missing->second + 1;
